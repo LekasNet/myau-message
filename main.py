cd4aebe13f667e72f31d3a6cf818a0f82bd54e34
Iiya Zakharov <2146376@gmail.com>
2024-05-22 02:09:07 +0300

    add func's for pulling requests

--- main.py
+++ main.py
@@ -1,13 +1,68 @@
+import re
+import string
+import nltk
+from nltk import WordNetLemmatizer
+from nltk.corpus import stopwords
+from pydantic import BaseModel
 from fastapi import FastAPI
+import requests
+import asyncio
+
+from sraca import predict
+
+nltk.download('stopwords')
+stop_words = set(stopwords.words('russian'))
+
+lemmatizer = WordNetLemmatizer()
+
+
+class Message(BaseModel):
+    theme: str
+    message: str
+
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def preprocess_text(text):
+    ban_word_twitch = string.punctuation + '—' + '«' + '»' + '...' + '“' + '”' + '„' + '‘' + '’'
+    pattern = f'[{re.escape(ban_word_twitch)}]|[0-9]|[a-zA-Z]'
+    text = re.sub(pattern, ' ', text)
+    text = text.lower()
+    words = nltk.word_tokenize(text)
+    words = [word for word in words if word not in stop_words]
+    words = [lemmatizer.lemmatize(word) for word in words]
+    text = ' '.join(words)
+    return text
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.get("/data")
+async def get_data():
+    response = requests.get("<ссылка на Лешен сервак>")
+    data = response.json()
+    response = await predict(data["theme"], data["massage"])
+    return {"response": response, "id": data["id"]}
+
+
+@app.post("/send_data")
+async def send_data(data: dict):
+    response = requests.post("<ссылка на Лешен сервак>", json=data)
+    if response.status_code == 200:
+        return {"message": "Data sent successfully"}
+    else:
+        return {"message": "Failed to send data"}
+
+
+async def poll_data():
+    while True:
+        data = await get_data()
+        await send_data(data)
+        await asyncio.sleep(0.5)
+
+
+async def main():
+    await asyncio.create_task(poll_data())
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
--- sraca.py
+++ sraca.py
@@ -1,32 +1,39 @@
 import torch
 from transformers import AutoModelForSequenceClassification, AutoTokenizer
+from dotenv import load_dotenv
+from icecream import ic
+
+load_dotenv()
+
+ic.enable()
 
 model = AutoModelForSequenceClassification.from_pretrained('my_model')
 tokenizer = AutoTokenizer.from_pretrained('my_model')
 
 
-themes = {1: "Программирование",
-          2: "Кино",
-          3: "Книги",
-          4: "Музыка",
-          5: "Игры",
-          6: "Юмор",
-          7: "Экономика",
-          8: "История"}
+themes = ["Программирование", "Кино", "Книги", "Музыка", "Игры", "Юмор", "Экономика", "История"]
 
 
-def predict(theme, message, threshold=0.5):
+async def predict(theme, message, threshold=0.5):
     inputs = tokenizer.encode_plus(
         f"{theme} [SEP] {message}",
         add_special_tokens=True,
         max_length=512,
+        truncation=False,
         return_attention_mask=True,
         return_tensors='pt'
     )
     with torch.no_grad():
         outputs = model(inputs['input_ids'], attention_mask=inputs['attention_mask'])
     probs = torch.sigmoid(outputs.logits)
+    ic(probs)
     probabilities = probs > threshold
+    ic(probabilities)
     pretion = probabilities.detach().numpy().flatten()
-    response = theme in [themes[i] for i in range(len(pretion)) if pretion[i] is True]
+    ic(pretion)
+    response = theme in [th for th in themes if pretion[themes.index(th)]==True]
+    ic(response)
     return response
+
+
+print(predict("Кино", "Актер сыграл отличную коль в фильме Мото Мото"))
