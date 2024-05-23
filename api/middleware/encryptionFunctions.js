ba6df213891e22f3c6192867069d180d044b79f8
N0tTryHard <billy.bones1@mail.ru>
2024-05-23 20:36:01 +0300

    hope encryption fixed.

--- api/middleware/encryptionFunctions.js
+++ api/middleware/encryptionFunctions.js
@@ -35,7 +35,7 @@ function aesEncrypt(text, hexKey) {
     const key = Buffer.from(hexKey, 'hex');
     const iv = Buffer.alloc(16); // Пока фиксированный вектор
     const cipher = crypto.createCipheriv('aes-256-cbc', key, iv);
-    let encrypted = cipher.update(text, 'utf8', 'hex');
+    let encrypted = cipher.update(text, 'ucs2', 'hex');
     encrypted += cipher.final('hex');
     return encrypted;
 }
@@ -45,7 +45,7 @@ function aesDecrypt(encrypted, hexKey) {
     const iv = Buffer.alloc(16); // Пока фиксированный вектор
     const decipher = crypto.createDecipheriv('aes-256-cbc', key, iv);
     let decrypted = decipher.update(encrypted, 'hex');
-    decrypted += decipher.final('utf8');
+    decrypted += decipher.final('ucs2');
     return decrypted;
 }
 
